686b80ab19b7985046120a7bf6bd3a984a8464b4
linkel <ironcarbide@gmail.com>
2019-08-13 11:01:31 -0700

    Refactor tests, add test for empty string parameter

--- hello.go
+++ hello.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello is a function that takes in a string and concatenates it with hello
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,12 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("you handsome delight")
-	want := "Hello, you handsome delight"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// t.Helper() tells the suite that this is a helper method
+		// so the reported failure will be in the function call
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("you handsome delight")
+		want := "Hello, you handsome delight"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 // 1. Tests need to be in a file with a name like blah_test.go
